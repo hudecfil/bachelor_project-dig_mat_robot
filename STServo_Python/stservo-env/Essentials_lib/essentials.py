a4cacca3fd8464b52c81ccbedf1a90c487df32c8
Filip Hudec <hudecfil@fel.cvut.cz>
2025-02-27 22:11:34 +0100

    mapping the angles in radians to servo steps doesnt match as it should

--- STServo_Python/stservo-env/Essentials_lib/essentials.py
+++ STServo_Python/stservo-env/Essentials_lib/essentials.py
@@ -17,9 +17,10 @@ MANIP_UP = 555
 
 # Servos zero position [steps]
 STS1_ZERO = 2100
-STS5_ZERO = 2050
 STS2_ZERO = 2050
-
+STS3_ZERO = 2100
+STS4_ZERO = 2175
+STS5_ZERO = 2050
 
 
 class Robot:
@@ -33,6 +34,8 @@ class Robot:
         self.scs_anchor_IDs = [6,7,9]
         self.scs_manip_ID = 8
         self.num_sts = len(self.sts_IDs)
+        self.sts_max_limit = []
+        self.sts_zero_points = [STS1_ZERO, STS2_ZERO, STS3_ZERO, STS4_ZERO, STS5_ZERO]
 
         # Open port
         if self.portHandler.openPort():
@@ -52,16 +55,41 @@ class Robot:
         # Close port
         self.portHandler.closePort()
 
-    def STS_rad_to_steps(self, rad):
+    def STS_rad_to_steps(self, servo_id, rad):
         """ Function maps input angle in radians to STS travel steps.
 
-            - Mid-point zero reference = 2048 steps
+            - Mid-point zero reference from self.sts_zero_points
             - CCW as positive rotation
             - Angle wrapped to range [-π, π] radians
         """
-        # Wrap angle to [-π, π] using modulo and atan2 trick
-        rad = (rad + np.pi) % (2 * np.pi) - np.pi
-        return int(2048 - (rad * (4096 / (2*np.pi))))
+        # Wrap angle to [-π, π]
+        rad = np.arctan2(np.sin(rad), np.cos(rad))
+
+        # Retrieve the correct zero point for the servo
+        zero_point = self.sts_zero_points[servo_id - 1]
+
+        # Convert radian to steps
+        steps = int(zero_point + (rad * (4096 / (2*np.pi))))
+
+        # Debugging output
+        print(f"Servo {servo_id} | Input rad: {rad:.4f} | Zero: {zero_point} | Steps: {steps}")
+
+        return steps
+
+    
+    def STS_steps_to_rad(self, servo_id, steps):
+        """ Converts STS travel steps to radians.
+
+            - Mid-point zero reference from self.sts_zero_points
+            - CCW as positive rotation
+            - Angle wrapped to range [-π, π] radians
+        """
+        zero_point = self.sts_zero_points[servo_id-1]
+
+        rad = (steps - zero_point)*((2*np.pi)/4096)
+
+        return rad
+
 
     def move_to_q(self, q: np.array = None):
         """ Move STS servos to the given configuration q .
@@ -72,12 +100,13 @@ class Robot:
         # Init configuration q
         if q is None:
             q = np.zeros(self.num_sts)
-        # Convert q from rad to steps
-        q = [self.STS_rad_to_steps(q_i) for q_i in q]
 
-        groupSyncRead = GroupSyncRead(self.sts, STS_PRESENT_POSITION_L, self.num_sts)
+        # Map q from rad to steps
+        q = [self.STS_rad_to_steps(i+1, q_i) for i, q_i in enumerate(q)]
 
-        for sts_id in range(1, self.num_sts):
+        groupSyncRead = GroupSyncRead(self.sts, STS_PRESENT_POSITION_L, 4)
+
+        for sts_id in self.sts_IDs:
             # Add STServo#1~10 goal position\moving speed\moving accc value to the Syncwrite parameter storage
             sts_addparam_result = self.sts.SyncWritePosEx(sts_id, q[sts_id-1], STS_MOVING_SPEED,
                                                                STS_MOVING_ACC)
@@ -93,7 +122,7 @@ class Robot:
         self.sts.groupSyncWrite.clearParam()
         time.sleep(0.002)  # wait for servo status moving=1
         while 1:
-            # Add parameter storage for STServo#1~10 present position value
+            # Add parameter storage for STServos
             for sts_id in self.sts_IDs:
                 sts_addparam_result = groupSyncRead.addParam(sts_id)
                 if sts_addparam_result != True:
@@ -112,7 +141,7 @@ class Robot:
                     sts_present_position = groupSyncRead.getData(sts_id, STS_PRESENT_POSITION_L, 2)
                     sts_present_speed = groupSyncRead.getData(sts_id, STS_PRESENT_SPEED_L, 2)
                     sts_present_moving = groupSyncRead.getData(sts_id, STS_MOVING, 1)
-                    # print(scs_present_moving)
+                    # print(sts_present_moving)
                     print("[ID:%03d] PresPos:%d PresSpd:%d" % (
                     sts_id, sts_present_position, self.sts.sts_tohost(sts_present_speed, 15)))
                     if sts_present_moving == 1:
@@ -129,11 +158,66 @@ class Robot:
             if sts_last_moving == 0:
                 break
 
-    def move_STS_step(self, servo_id=1, steps=2048):
-        # Default setting
-        #STS_MINIMUM_POSITION_VALUE  = 0           # STServo will rotate between this value
-        #STS_MAXIMUM_POSITION_VALUE  = 2048        # ST = 4095 max position
+    def get_q(self):
+        cur_q = np.zeros(self.num_sts)
 
+        groupSyncRead = GroupSyncRead(self.sts, STS_PRESENT_POSITION_L, 4)
+
+        for sts_id in self.sts_IDs:
+            # Add parameter storage for STServos
+            sts_addparam_result = groupSyncRead.addParam(sts_id)
+            if sts_addparam_result != True:
+                print("[ID:%03d] groupSyncRead addparam failed" % sts_id)
+
+        sts_comm_result = groupSyncRead.txRxPacket()
+        if sts_comm_result != COMM_SUCCESS:
+            print("%s" % self.sts.getTxRxResult(sts_comm_result))
+
+        for sts_id in self.sts_IDs:
+            # Check if groupsyncread data of STServos
+            sts_data_result, sts_error = groupSyncRead.isAvailable(sts_id, STS_PRESENT_POSITION_L, 4)
+            if sts_data_result == True:
+                # Get STServo#scs_id present position value
+                sts_present_position = groupSyncRead.getData(sts_id, STS_PRESENT_POSITION_L, 2)
+                cur_q[sts_id-1] = self.STS_steps_to_rad(sts_id, sts_present_position)
+                print("[ID:%03d] PresPos:%d " % (sts_id, sts_present_position))
+            else:
+                print("[ID:%03d] groupSyncRead getdata failed" % sts_id)
+                continue
+            if sts_error != 0:
+                print("%s" % self.sts.getRxPacketError(sts_error))
+
+        groupSyncRead.clearParam()
+        return cur_q
+
+    def move_STS_step(self, servo_id=1, steps=2048):
+        # Write STServo goal position/moving speed/moving acc
+        sts_comm_result, sts_error = self.sts.WritePosEx(servo_id, steps, STS_MOVING_SPEED, STS_MOVING_ACC)
+        if sts_comm_result != COMM_SUCCESS:
+            print("%s" % self.sts.getTxRxResult(sts_comm_result))
+        elif sts_error != 0:
+            print("%s" % self.sts.getRxPacketError(sts_error))
+
+        while 1:
+            # Read STServo present position
+            sts_present_position, sts_present_speed, sts_comm_result, sts_error = self.sts.ReadPosSpeed(servo_id)
+            if sts_comm_result != COMM_SUCCESS:
+                print(self.sts.getTxRxResult(sts_comm_result))
+            else:
+                print("[ID:%03d] GoalPos:%d PresPos:%d PresSpd:%d" % (servo_id, steps, sts_present_position, sts_present_speed))
+            if sts_error != 0:
+                print(self.sts.getRxPacketError(sts_error))
+
+            # Read STServo moving status
+            moving, sts_comm_result, sts_error = self.sts.ReadMoving(servo_id)
+            if sts_comm_result != COMM_SUCCESS:
+                print(self.sts.getTxRxResult(sts_comm_result))
+
+            if moving==0:
+                break
+
+    def move_STS_rad(self, servo_id=1, rad=np.pi/4):
+        steps = self.STS_rad_to_steps(servo_id, steps)
         # Write STServo goal position/moving speed/moving acc
         sts_comm_result, sts_error = self.sts.WritePosEx(servo_id, steps, STS_MOVING_SPEED, STS_MOVING_ACC)
         if sts_comm_result != COMM_SUCCESS:
--- STServo_Python/stservo-env/Essentials_lib/main.py
+++ STServo_Python/stservo-env/Essentials_lib/main.py
@@ -5,4 +5,12 @@ robot = Robot()
 #robot.grab_rel_voxel(grab=True)
 #robot.grab_rel_voxel(grab=False)
 
-robot.lock_anchor(7, False)
+cur_q = robot.get_q()
+print("Current configuration: ", cur_q)
+
+cur_q_steps = [robot.STS_rad_to_steps(i+1, q_i) for i, q_i in enumerate(cur_q)]
+print("Current computed configuration: ", cur_q_steps)
+#robot.move_STS_step(2, 3125)
+#robot.move_STS_step(2, 2952)
+
+
